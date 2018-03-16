c5b3ee4ee03bedf8b8ffca0eb2d93ffe51438b90
choasup <yong.liu@vipl.ict.ac.cn>
2018-03-16 11:31:00 +0800

    Update test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -1,1 +1,1 @@
-./experiments/scripts/test_faster_rcnn_end2end.sh gpu 0 VGG16 pascal_voc 2>&1 | tee test.log.07++12
+./experiments/scripts/test_faster_rcnn_end2end.sh gpu 0 VGG16 pascal_voc 2>&1 | tee test.log.07
